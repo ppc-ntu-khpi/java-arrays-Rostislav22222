3c7bd70a30782fd8ba28439598539f20ed45aeb5
Галанін Ростислав <119488812+Rostislav22222@users.noreply.github.com>
2023-05-05 10:33:26 +0300

    Add files via upload

---
+++ src/TestResult.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+// package test;
+
+// import domain.Exercise;
+
+public class TestResult {
+public static void main(String[] args) {
+Exercise ggbot = new Exercise();
+ggbot.calculate();
+}
+}
